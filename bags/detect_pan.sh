6217a90c3d41ba29ccf51232b257933e46890de9
HiroIshida <spitfire.docomo@gmail.com>
2021-05-23 13:41:36 +0900

    Publish mask polygon

--- bags/detect_pan.sh
+++ bags/detect_pan.sh
@@ -1,8 +1,10 @@
 # note that edgetpu_object_detector's input is /kinect_head/rgb/image_rect_color
-rosbag record /kinect_head/depth_registered/half/points \
+rosbag record \
+    /kinect_head/depth_registered/points \
     /kinect_head/rgb/image_rect_color \
     /edgetpu_object_detector/output/class \
     /edgetpu_object_detector/output/image \
     /edgetpu_object_detector/output/rects \
+    /kinect_head/rgb/camera_info \
     /tf \
     -O ./pan_detect.bag
