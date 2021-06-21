73e8cc056d1dd5d05a822005b25f1bdf70790ec1
HiroIshida <spitfire.docomo@gmail.com>
2021-06-22 01:42:25 +0900

    Tweak

--- main_script/demo.py
+++ main_script/demo.py
@@ -11,6 +11,8 @@ from scipy.spatial.transform import Rotation as R
 from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg import PointStamped
 
+from reproduce_trajectory import Reproducer
+
 def translate_robot(robot_model, joint_list, trans):
     assert len(trans)==2
     av_current = get_robot_config(robot_model, joint_list, with_base=True)
@@ -43,7 +45,7 @@ class Demo(object):
 
 
         rospy.Subscriber("grasp_pose", PoseStamped, self._callback_grasp_pose)
-        rospy.Subscriber("pan_surface_center", PointStamped, self._callback_point)
+        rospy.Subscriber("pan_surface_center", PoseStamped, self._callback_pan_center)
 
         # set in _callback_grasp_pose
         self._target_grasp_pose = None
@@ -69,9 +71,11 @@ class Demo(object):
         target_pose.translate([-0.05, 0, 0])
         self._target_pose = target_pose
 
-    def _callback_point(self, msg):
-        point = np.array([msg.point.x, msg.point.y, msg.point.z])
-        self._pan_surface_center = point
+    def _callback_pan_center(self, msg):
+        pose = msg.pose 
+        pos = [pose.position.x, pose.position.y, pose.position.z]
+        ori = [pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w]
+        self._pan_surface_center = [pos, ori]
 
     def obtain_current_angle_vector(self):
         self.robot_model.angle_vector(self.ri.angle_vector())
@@ -97,10 +101,13 @@ class Demo(object):
         angles = np.array([-28.31, -5.975, -111.2, -56.61, -53.92, -63.41, -172.148]) * math.pi/180.0
         self.set_rarm(angles)
         res = self.robot_model.inverse_kinematics(self._target_pose, link_list=self.link_list, move_target=self.rarm_end_coords, rotation_axis=True)
+        assert res is not False
+
         self.ri.angle_vector(self.robot_model.angle_vector(), time=2.5, time_scale=1.0)
         self.ri.wait_interpolation()
 
         res = self.robot_model.inverse_kinematics(self._target_grasp_pose, link_list=self.link_list, move_target=self.rarm_end_coords, rotation_axis=True)
+        assert res is not False
         self.ri.angle_vector(self.robot_model.angle_vector(), time=1.0, time_scale=1.0)
         self.ri.wait_interpolation()
         self.ri.move_gripper("rarm", pos=0.0)
@@ -111,51 +118,15 @@ class Demo(object):
             viewer.add(Axis.from_coords(self._target_grasp_pose))
             viewer.show()
 
-    def move_to_pan(self, use_real_robot=False, debug=False, angles=[90, 45, 90]):
-        # DEBUG!!!!!
-        self._pan_surface_center = np.array([0.7908201515525104, 0.23566020955566158, 0.9293659055940292])
+    def place_egg_on_pan(self):
+        assert self._pan_surface_center is not None
+        repro = Reproducer(self.robot_model, "../aux_script/trajectory.json", use_torso=False)
+        angles_list, joint_names = repro(T_target_to_base)
 
-        assert self._pan_surface_center is not None, "reaching target is not set yet"
-
-        rarm_angles = [0.12, 0.044, -1.28, -1.198, -1.5, -1.67, -3.38]
-        (j.joint_angle(a) for j, a in zip(self.joint_list, rarm_angles))
-
-        offset = np.array([0.0, 0.1, 0.1])
-        position = self._pan_surface_center + offset
-
-        if use_real_robot:
-            #demo.ri.go_pos_unsafe_no_wait(*[0.0, 0.5, 0.0])
-            pass
-        else:
-            translate_robot(self.robot_model, self.joint_list, [0.1, 0.5])
-
-        def solve_ik(angles):
-            r = R.from_euler('xyz', angles, degrees=True)
-            x, y, z, w = r.as_quat() # TODO can be simpler by using as_matrix
-            orientation = np.array([w, x, y, z])
-            rotmat = skrobot.coordinates.math.rotation_matrix_from_quat(orientation)
-            prepare_pose = skrobot.coordinates.Coordinates(position, rotmat)
-
-
-            res = self.robot_model.inverse_kinematics(prepare_pose, link_list=self.link_list, move_target=self.rarm_end_coords, rotation_axis=True)
-
-        for a in [60, 30, 0, -30, -60, -90]:
-            solve_ik([a, 45, 30])
-
-            if use_real_robot:
-                self.ri.angle_vector(self.robot_model.angle_vector(), time=0.5, time_scale=1.0)
-                time.sleep(0.3)
-
-        solve_ik([-90, -10, 30])
-        if use_real_robot:
-            self.ri.angle_vector(self.robot_model.angle_vector(), time=0.5, time_scale=1.0)
-            self.ri.wait_interpolation()
-
-        if debug:
-            viewer = skrobot.viewers.TrimeshSceneViewer(resolution=(640, 480))
-            viewer.add(self.robot_model)
-            viewer.add(Axis.from_coords(prepare_pose))
-            viewer.show()
+        for angles in angles_list:
+            for jn, a in zip(joint_names, angles):
+                self.robot_model.__dict__[jn].joint_angle(a)
+            self.ri.angle_vector(self.robot_model.angle_vector(), time=1.0, time_scale=1.0)
 
 try:
     demo
